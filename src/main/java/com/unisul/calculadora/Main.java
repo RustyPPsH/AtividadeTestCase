e2807b03d9297250f04ba57b5d6dc563dba7c08d
Rafael Sowgo Zamora Sonoki <108179227+RustyPPsH@users.noreply.github.com>
2024-05-28 03:11:57 -0300

    Add files via upload

---
+++ src/main/java/com/unisul/calculadora/Main.java
@@ -0,0 +1,41 @@
+package com.unisul.calculadora;
+
+import javax.swing.JOptionPane;
+import com.unisul.calculadora.Calculadora;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        String opcao = "";
+        Calculadora calculadora = new Calculadora();
+
+        while (!opcao.equals("9")) {
+            opcao = JOptionPane.showInputDialog("1 - Leitura \n2 - Soma \n3 - Subtração \n4 - Produto \n5 - Divisão \n9 - Sair");
+
+            switch (Integer.parseInt(opcao)) {
+                case 1:
+                    calculadora.setValorA(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de A: ")));
+                    calculadora.setValorB(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de B: ")));
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Resultado: " + calculadora.getAdicao());
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Resultado: " + calculadora.getSubtracao());
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null, "Resultado: " + calculadora.getProduto());
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, "Resultado: " + calculadora.getDivisao());
+                    break;
+                case 9:
+                    JOptionPane.showMessageDialog(null, "Saindo...");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida!");
+            }
+
+        }
+    }
+}
